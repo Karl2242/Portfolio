ca05a09fecacfd7b8e5b55ae094fcd5a9c8b855d
Karl2242 <kchikhicloud@gmail.com>
2025-02-25 10:24:15 +0100

    add desc

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio de Karl Chikhoun - Apprenti Developer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    `🚀 Salut, moi c'est Karl Chikhoun !  
+Développeur Web Junior passionné, je n'ai jamais cessé d'apprendre.  
+Curieux de nature, j'ai une soif insatiable de connaissances et une envie constante de repousser mes limites.  
+
+💡 Explorer, comprendre, créer. C'est ce qui me motive au quotidien.  
+
+Prêt à relever de nouveaux défis et à bâtir des projets innovants ! 🔥`,
 };
 
 export default function RootLayout({ children }) {
