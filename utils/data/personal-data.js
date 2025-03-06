1e1eb93df7e6aff48311a7f49cb72ecd0e6b31d0
Karl2242 <kchikhicloud@gmail.com>
2025-03-06 16:09:04 +0100

    NEW: Change text

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "Mon nom est Karl Chikhoun. Je suis actuellement en formation chez Garage404, où j’apprends le métier de développeur web. Passionné par ce domaine, j’aime découvrir de nouvelles choses, ce qui nourrit ma curiosité. Mon objectif est de continuer à apprendre et à me perfectionner constamment.",
   email: 'kchikhicloud@gmail.com',
   phone: '+330658756825',
-  address: '17 Avenue Berthelot',
+  address: 'Saint Etienne-Lyon-Saint Chamond-',
   github: 'https://github.com/Karl2242',
   facebook: '',
   linkedIn: 'https://www.linkedin.com/in/karl-chikhoun-2a0baa2b2/',
